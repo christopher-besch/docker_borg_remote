3493680f9e13d783db7f22c67883e0f459c8987e
christopher-besch <mail@chris-besch.com>
2022-10-01 10:20:48 +0200

    now using borg 2.0

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # docker_borg_remote
 Using Borg and Docker to sync Backups.
+This uses `borg transfer`, which is a feature of borg 2.0.
 
 - [docker_borg_server](https://hub.docker.com/r/chrisbesch/docker_borg_server)
 - [docker_borg_client](https://hub.docker.com/r/chrisbesch/docker_borg_client)
--- client/borg_client.sh
+++ client/borg_client.sh
@@ -4,89 +4,38 @@ set -euo pipefail
 IFS=$' \n\t'
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-# $1 server repo::archive
-# $2 client repo::archive
-function pull_archive() {
-    # in case temp dir hasn't been mounted with docker
-    mkdir -p /origin
-    cd /origin
-    rm -rf /origin/*
-
-    borg extract --rsh "$RSH" $1
-
-    # borg on server already creates an origin directory
-    if [ ! -z ${DEL_ORIGIN_DIR+x} ]; then
-        if [ -d /origin/origin ]; then
-            echo "remove origin dir"
-            mv origin/* .
-            rm -d origin
-        else
-            echo "Error trying to remove origin dir failed because it doesn't exist"
-        fi
-    else
-        echo "don't remove origin dir"
-    fi
-
-    borg create --info --compression $BORG_COMPRESSION "$2" /origin
-}
-
 function prune_and_compact() {
     if [ ! -z ${PRUNE_CFG+x} ]; then
         echo "running: borg prune $PRUNE_CFG $1"
-        borg prune $PRUNE_CFG $1
+        borg -r $1 prune $PRUNE_CFG
     else
         echo "PRUNE_CFG not defined"
     fi
 
     echo "compacting borg repo"
-    borg compact $1
-}
-
-# $1 archive
-# $2 client archives
-function is_archive_present() {
-    for CLIENT_ARCHIVE in $2; do
-        if [[ $CLIENT_ARCHIVE == $1 ]]; then
-            return 0
-        fi
-    done
-    return 1
+    borg -r $1 compact
 }
 
 function backup_repo() {
-    SERVER_REPO="root@${BORG_SERVER}:/var/lib/borg_server/repos/$1"
+    SERVER_REPO="ssh://root@${BORG_SERVER}/var/lib/borg_server/repos/$1"
     CLIENT_REPO="/var/lib/borg_client/repos/$1"
     echo "start backup of repo '$1' at $(date)"
 
     # validate repos
-    if ! borg check --rsh "$RSH" $SERVER_REPO; then
+    if ! borg -r $SERVER_REPO check --rsh "$RSH"; then
         echo "ERROR: borg server repo '$1' invalid"
         return
     fi
     echo "borg server repo '$1' valid"
-    if ! borg check $CLIENT_REPO; then
+    if ! borg -r $CLIENT_REPO check; then
         echo "ERROR: borg client repo '$1' invalid"
         return
     fi
     echo "borg client repo '$1' valid"
 
-    # get archive list
-    SERVER_ARCHIVES=$(borg list --short --rsh "$RSH" $SERVER_REPO)
-    echo "found server archives:"
-    echo $SERVER_ARCHIVES
-    CLIENT_ARCHIVES=$(borg list --short $CLIENT_REPO)
-    echo "found client archives:"
-    echo $CLIENT_ARCHIVES
-
-    # pull required archives
-    for ARCHIVE in $SERVER_ARCHIVES; do
-        if is_archive_present $ARCHIVE "$CLIENT_ARCHIVES"; then
-            echo "archive '$ARCHIVE' from '$1' is already present on client"
-        else
-            echo "pulling archive '$ARCHIVE' from '$1'"
-            pull_archive "$SERVER_REPO::$ARCHIVE" "$CLIENT_REPO::$ARCHIVE"
-        fi
-    done
+    borg -r $CLIENT_REPO transfer --other-repo $SERVER_REPO --dry-run
+    borg -r $CLIENT_REPO transfer --other-repo $SERVER_REPO
+    borg -r $CLIENT_REPO transfer --other-repo $SERVER_REPO --dry-run
 
     prune_and_compact $CLIENT_REPO
 
