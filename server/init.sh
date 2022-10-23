300b375b05981f70338d534ae6ccd2bda697811f
christopher-besch <mail@chris-besch.com>
2022-10-23 14:08:45 +0200

    working on fixing transfer

--- server/init.sh
+++ server/init.sh
@@ -6,6 +6,9 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # restrict to borg server command
 sed 's@^ssh-rsa@command="borg serve --restrict-to-path /var/lib/borg_server/repos",restrict ssh-rsa@g' /var/lib/borg_server/authorized_keys > /root/.ssh/authorized_keys
+# for debugging
+# cat /var/lib/borg_server/authorized_keys > /root/.ssh/authorized_keys
+cat /root/.ssh/authorized_keys
 
 # don't detach and print log into stdout
 /usr/sbin/sshd -De
