ddfa975ab89d72d0b4fd087565fbed205f790a70
christopher-besch <mail@chris-besch.com>
2022-10-23 12:35:38 +0200

    updated server

--- server/init.sh
+++ server/init.sh
@@ -7,5 +7,6 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # restrict to borg server command
 sed 's@^ssh-rsa@command="borg serve --restrict-to-path /var/lib/borg_server/repos",restrict ssh-rsa@g' /var/lib/borg_server/authorized_keys > /root/.ssh/authorized_keys
 
+# don't detach and print log into stdout
 /usr/sbin/sshd -De
 
